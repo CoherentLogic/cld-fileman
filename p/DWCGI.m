e7438faf1ccac5d36ede0291c16ad30e6bb6ff4d
chocolatejollis38@gmail.com <chocolatejollis38@gmail.com@7140fc68-708c-eff2-3006-acd0cb7ec3c7>
2011-08-30 13:55:56 +0000

    Initial import.

---
+++ p/DWCGI.m
@@ -0,0 +1,57 @@
+DWCGI
+ N DWQS,IEN,TP
+ S QS=$ZTRNLNM("QUERY_STRING")
+ F I=1:1:$L(QS,"&")  D
+ .S TP=$P(QS,"&",I)
+ .S DWQS($P(TP,"=",1))=$P(TP,"=",2)
+ I DWQS("action")="get-json-record" D JSON
+ I DWQS("action")="get-field" D GETFIELD
+ I DWQS("action")="set-field" D SETFIELD
+ I DWQS("action")="field-list" D FLDLIST
+ I DWQS("action")="file-numbers" D FILENUMS(DWQS("instance"))
+ I DWQS("action")="authenticate" D AUTH(DWQS("session_id"),DWQS("access_code"))
+ Q
+
+AUTH(SESSID,ACCODE)
+ N OK S OK=0
+ N X S X=ACCODE
+ I X'="" D
+ .D ^XUSHSH S DUZ=$O(^VA(200,"A",X,0))
+ .I DUZ'="" S OK=1
+ I OK=1 D
+ .S ^DWSESS(SESSID,"ACCESS CODE")=ACCODE
+ D HEADER("text/plain")
+ W OK
+ Q
+
+FLDLIST
+ N FL,CNT
+ Q
+
+GETFIELD
+ W !
+ Q
+
+SETFIELD
+ W !
+ Q
+
+FILENUMS(INSTANCE) 
+ N FILES S FILES=""
+ N CNT S CNT=$$FILELIST^DWDIQ(.FILES)
+ D HEADER("text/plain")
+ F I=1:1:CNT D
+ .D ARRELEM^DWPHP(INSTANCE_"->files",FILES(I,"DDNUM"),FILES(I,"NAME"))
+ Q
+ 
+
+JSON
+ D HEADER("application/json")
+ D GET^DWFMJSON(^DWQS("dd"),^DWQS("ien"))
+ Q
+
+HEADER(CTYPE)
+ W "Content-type: ",CTYPE,!,!
+ Q
+
+ 
---
+++ p/DWFMJSON.m
@@ -0,0 +1,43 @@
+DWFMJSON
+
+GET(FILENUM,IEN)
+ N FIELDS
+ N CNT
+ N VAL
+ N IENS
+ S IENS=IEN_","
+ S CNT=$$FLDNAMES^DWDIQ(FILENUM,.FIELDS)
+ D OPEN
+ D HEADER(FILENUM,IEN)
+ F I=1:1:CNT  D
+ .S VAL=$$GET1^DIQ(FILENUM,IENS,FIELDS(I))
+ .D PAIR(FIELDS(I),VAL)
+ D ENDPAIR(FILENUM,IEN)
+ D CLOSE
+ Q  
+
+OPEN
+ W "{",!
+ Q
+
+CLOSE
+ W "}"
+ Q
+
+HEADER(FILENUM,IEN)
+ D PAIR("dd-num",FILENUM)
+ D PAIR("ien",IEN)
+ Q
+
+PAIR(KEY,VALUE)
+ N ST
+ S ST=""""_KEY_""""_": "_""""_VALUE_""""_","
+ W ST,!
+ Q
+
+ENDPAIR(FILENUM,IEN)
+ N ST
+ S ST="""end-record"""_": "_""""_FILENUM_"-"_IEN_""""
+ W ST,!
+ Q
+
