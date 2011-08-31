f1c2c7afc1a371d6fefaf33ad6874b0c7eee3c67
chocolatejollis38@gmail.com <chocolatejollis38@gmail.com@7140fc68-708c-eff2-3006-acd0cb7ec3c7>
2011-08-31 18:25:04 +0000

    Daily commit.

--- trunk/html/js/fileman.js
+++ trunk/html/js/fileman.js
@@ -3,7 +3,7 @@
  */
 
 
-function LoadFieldList()
+function LoadFieldsFromSelection()
 {
     var CurrentFile = $('#MasterFilesList').val();
     var URL = 'fieldlist.php?dd=' + escape(CurrentFile);
@@ -12,11 +12,22 @@ function LoadFieldList()
     });
 }
 
+function LoadFieldList(file_number)
+{
+    var URL = 'fieldlist.php?dd=' + escape(file_number);
+    $.get(URL, function(data) {
+	$('[id="field_list"]').html(data);
+    });
+
+    $("#MasterFilesList").val(file_number);
+
+}
+
 function LoadFile(file_number, field_number)
 {
     //alert("dd: " + file_number + " field: " + field_number);
 
-    var URL = 'file.php?dd=' + escape(file_number) + '&field=' + field_number;
+    var URL = 'field.php?dd=' + escape(file_number) + '&field=' + field_number;
     $.get(URL, function(data) {
 	$('[id="ContentArea"]').html(data);
     });
